ce75d793dc39faf5047caae1250dece5be56627e
yasyars <yasya.rusyda@gmail.com>
2021-09-19 15:12:31 +0700

    still writing solution

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static int[][] afterLife(int[][] initial){
-        int[][] newMatrix = int[len]
+        int m= initial.length;
+        int n= initial[0].length;
+        int[][] newMatrix = new int[m][n];
+
+        for (int i=0;i<m; i++){
+            for (int k=0; k<n; k++){
+                int neighbour = 0;
+                if (i>0 && k>0){
+                    neighbour+= initial[i-1][k-1];
+                    }
+                }
+                if (i<)
+
+
+            }
+        }
+
+        return newMatrix;
     }
 
     public static void main(String args[]){
