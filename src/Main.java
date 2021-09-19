3cde0147cc65c4c36ff2e544962931d5142cc957
yasyars <yasya.rusyda@gmail.com>
2021-09-19 14:45:54 +0700

    input matrix

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
-class Board {
+import java.util.Scanner;
 
-}
-
-class Cell{
-
-}
 public class Main {
     public static void main(String args[]){
+        Scanner scan = new Scanner (System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
 
+        int[][] matrix = new int[m][n];
+        for (int i=0; i<m; i++){
+            int[] array = new int[n];
+            for (int k=0; k<n; k++){
+                array[k] = scan.nextInt();
+            }
+            matrix[i] = array;
+        }
     }
 }
