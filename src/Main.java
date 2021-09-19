0e70387fc5aaa6a8ad4387131d52250d421a2054
yasyars <yasya.rusyda@gmail.com>
2021-09-19 22:02:04 +0700

    Refactor for better solution

--- src/Main.java
+++ src/Main.java
@@ -1,89 +1,92 @@
 import java.util.Scanner;
 
 class Grid{
-  private int row;
-  private int column;
-  private int[][] cells;
+    private int row;
+    private int column;
+    private int[][] cells;
 
-  public Grid(int row, int column){
+    public Grid(int row, int column){
       cells = new int[row][column];
-  }
-
-  public void populate(){
-      Scanner scan = new Scanner (System.in);
-
-      for (int i=0; i<this.row; i++){
-          int[] array = new int[this.column];
-          for (int k=0; k<this.column; k++){
-              array[k] = scan.nextInt();
-          }
-          this.cells[i] = array;
-      }
-  }
-
-  public void print(){
-    for (int i=0;i<this.row; i++){
-        for (int k=0; k<this.column; k++){
-            System.out.print(this.cells[i][k] + " ");
-        }
-        System.out.print("\n");
     }
-  }
 
-  public static int[][] afterLife(int[][] initial){
-        int m= initial.length;
-        int n= initial[0].length;
-        int[][] newMatrix = new int[m][n];
+    public void populate(){
+        Scanner scan = new Scanner (System.in);
 
-        for (int i=0;i<m; i++){
-            for (int k=0; k<n; k++){
+        for (int i=0; i<this.row; i++){
+            int[] array = new int[this.column];
+            for (int k=0; k<this.column; k++){
+                array[k] = scan.nextInt();
+        }
+        this.cells[i] = array;
+        }
+    }
+
+    public void populateWithArray(int[][] newCells){
+        for (int i=0; i<this.row; i++){
+            for (int k=0; k<this.column; k++){
+                this.cells[i][k]= newCells[i][k];
+            }
+        }
+    }
+
+    public void print(){
+        for (int i=0;i<this.row; i++){
+            for (int k=0; k<this.column; k++){
+                System.out.print(this.cells[i][k] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public void evolve(){
+        int[][] newCells = new int[this.row][this.column];
+
+        for (int i=0;i<this.row; i++){
+            for (int k=0; k<this.column; k++){
                 int neighbour = 0;
                 if (i>0 && k>0) {
-                    neighbour += initial[i - 1][k - 1];
+                    neighbour += this.cells[i - 1][k - 1];
                 }
 
-                if (i<m-1 && k<n-1) {
-                    neighbour += initial[i+1][k+1];
+                if (i<this.row-1 && k<this.column-1) {
+                    neighbour += this.cells[i+1][k+1];
                 }
 
-                if (i>0 && k<n-1){
-                    neighbour += initial[i-1][k+1];
+                if (i>0 && k<this.column-1){
+                    neighbour += this.cells[i-1][k+1];
                 }
 
-                if (i<m-1 && k>0){
-                    neighbour += initial[i+1][k-1];
+                if (i<this.row-1 && k>0){
+                    neighbour += this.cells[i+1][k-1];
                 }
 
                 if (i>0){
-                    neighbour += initial[i-1][k];
+                    neighbour += this.cells[i-1][k];
                 }
                 if (k>0){
-                    neighbour += initial[i][k-1];
+                    neighbour += this.cells[i][k-1];
                 }
-                if (i<m-1){
-                    neighbour += initial[i+1][k];
+                if (i<this.row-1){
+                    neighbour += this.cells[i+1][k];
                 }
-                if(k<n-1){
-                    neighbour += initial[i][k+1];
+                if(k<this.column-1){
+                    neighbour += this.cells[i][k+1];
                 }
 
                 if (neighbour<2 || neighbour>3){
-                    newMatrix[i][k]=0;
+                    newCells[i][k]=0;
                 }else if (neighbour==3){
-                    newMatrix[i][k]=1;
+                    newCells[i][k]=1;
                 }else{//if 2
-                    newMatrix[i][k]= initial[i][k];
+                    newCells[i][k]= this.cells[i][k];
                 }
             }
         }
-
-        return newMatrix;
+        populateWithArray(newCells);
     }
 }
 
 public class Main {
-
-
     public static void main(String args[]){
         Scanner scan = new Scanner (System.in);
         int m = scan.nextInt();
@@ -96,7 +99,8 @@ public class Main {
         lifeGrid.print();
 
         System.out.println("\nAfter a life..");
-        printMatrix(afterLife(matrix));
+        lifeGrid.evolve();
+        lifeGrid.print();
 
     }
 }
